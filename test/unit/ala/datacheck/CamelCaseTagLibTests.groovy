4bbc8539f6864f5a0148b677c6790ff73af50ad2
djtfmartin <djtfmartin@gmail.com>
2011-09-07 04:06:56 +0000

    enhancements to initial attempt
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/ala-datacheck@2686 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ test/unit/ala/datacheck/CamelCaseTagLibTests.groovy
@@ -0,0 +1,17 @@
+package ala.datacheck
+
+import grails.test.*
+
+class CamelCaseTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
