e68f9aaebff8c41e568e67e20c219613d7b467e6
Simon Bear <simon.bear@csiro.au>
2016-07-13 17:07:48 +1000

    #26 Sandbox preview

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -47,26 +47,6 @@
 
   sandbox.value('sandboxConfig', config);
 
-  sandbox.run(
-    [          '$rootScope', '$state', '$stateParams',
-      function ($rootScope,   $state,   $stateParams) {
-
-        // It's very handy to add references to $state and $stateParams to the $rootScope
-        // so that you can access them from any scope within your applications.For example,
-        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-        // to active whenever 'contacts.list' or one of its decendents is active.
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
-
-        $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-          event.preventDefault();
-          $state.go('preview');
-          $
-        });
-      }
-    ]
-  );
-
   sandbox.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
     cfpLoadingBarProvider.includeSpinner = false;
     // cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
@@ -85,7 +65,27 @@
         $urlRouterProvider
           .when('/dataCheck/index', '/dataCheck')
           .when('/my-data', '/datasets/'+config.userId)
-          .otherwise('/datasets/'+config.userId);
+          .otherwise('/dataCheck');
+      }
+    ]
+  );
+
+  sandbox.run(
+    [          '$rootScope', '$state', '$stateParams',
+      function ($rootScope,   $state,   $stateParams) {
+
+        // It's very handy to add references to $state and $stateParams to the $rootScope
+        // so that you can access them from any scope within your applications.For example,
+        // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+        // to active whenever 'contacts.list' or one of its decendents is active.
+        $rootScope.$state = $state;
+        $rootScope.$stateParams = $stateParams;
+
+        $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+          event.preventDefault();
+          $state.go('preview');
+          $
+        });
       }
     ]
   );
