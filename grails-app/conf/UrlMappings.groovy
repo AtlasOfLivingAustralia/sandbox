a0e034e0fa5e2f8a1f69a31de87d0fee9fc4c328
Javier Molina <Javier.Molina@csiro.au>
2016-10-10 11:30:19 +1100

    #38 - Final cleanup

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -11,7 +11,6 @@ class UrlMappings {
 		"/jenkins/console/$jobName/$id/$start"(controller: 'jenkins', action: 'console')
 
 //		"/layers/$tempUid"(controller: "myDatasets", action: "layers")
-//		"/charts/$tempUid"(controller: "myDatasets", action: "chartOptions")
 		"/"(controller: "dataCheck", view:"/dataCheck/index")
 		"500"(view:'/error')
 	}
